4ab80935307aa982455c845f0c0d17947164c917
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-05-29 14:22:24 +0200

    Pruebas login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './jwt.strategy';
+import { jwtConstants } from './constantes';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
-      secret: 'my-secret-key', 
+      secret: jwtConstants.secret, 
       signOptions: { expiresIn: '1d' },
     }),
   ],
